8f59b22ad5a74645d3768e8d598555dbfa387fd2
Sergei Mamykin <mamykin@oborot.ru>
2019-06-10 20:32:36 +0300

    WIP | Настройка вебпак

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./src/app.js",
+        styles: ".src/main.scss"
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["es2015"] }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    }
+};
