17d5a908f11e797989b008ccc35e422091c897e5
Sergei Mamykin <mamykin@oborot.ru>
2019-06-11 18:42:17 +0300

    Настройка вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: {
         app: "./src/app.js",
-        styles: ".src/main.scss"
+        styles: "./src/main.scss"
     },
     devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        // new CleanWebpackPlugin(),
+        // new HtmlWebpackPlugin({title: 'test'})
+    ],
     devServer: {
         contentBase: './dist'
     },
@@ -18,9 +24,10 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: "babel-loader",
-                    options: { presets: ["es2015"] }
+                    options: {presets: ['@babel/preset-env']}
                 }
             },
             {
@@ -35,6 +42,12 @@ module.exports = {
                     {
                         loader: "sass-loader" // compiles Sass to CSS
                     }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
                 ]
             }
         ]
