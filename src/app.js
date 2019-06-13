5f8fc0eb3bf0f279487312ed71085eb70df02d7a
Sergei Mamykin <mamykin@oborot.ru>
2019-06-13 19:30:56 +0300

    немного заверстал плашечки на главной + плавный скрол

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 import Swiper from 'swiper';
 
+//swiper
 new Swiper('.swiper-container', {
     speed: 600,
     parallax: true,
     loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -17,28 +14,52 @@ new Swiper('.swiper-container', {
         disableOnInteraction: false,
     }
 });
+
+//hide header for cover picture
 const showHideBoundary = 100;
-let header = document.querySelector('.header');
-const hideHeader = (header) => {
-    if (showHideBoundary > window.scrollY) {
+const header = document.querySelector('.header');
+const hideHeader = (header, scrolledContainer) => {
+    if (showHideBoundary > scrolledContainer.scrollTop) {
         header.style.top = '-100px';
     }
 };
-const showHeader = (header) => {
-    if (showHideBoundary < window.scrollY) {
+const showHeader = (header, scrolledContainer) => {
+    if (showHideBoundary < scrolledContainer.scrollTop) {
         header.style.top = '0px';
     }
 };
-const hideShowHeader = (header) => {
-    hideHeader(header);
-    showHeader(header);
+const hideShowHeader = (header, scrolledContainer) => {
+    hideHeader(header, scrolledContainer);
+    showHeader(header, scrolledContainer);
 };
 
-hideShowHeader(header);
+const body = document.querySelector('body');
 
-const onScroll = () => {
+hideShowHeader(header, body);
+
+const onScroll = (event) => {
     let header = document.querySelector('.header');
-    hideShowHeader(header);
+    hideShowHeader(header, event.target);
 };
 
-document.addEventListener('scroll', onScroll);
+body.addEventListener('scroll', onScroll);
+
+
+//scroll further from a cover picture
+const scrollLink = document.querySelector('.js-scroll-swiper-link');
+const onClickOnScrollLink = () => {
+    let id = setInterval(function(){
+        const end  = scrollLink.offsetTop - header.offsetHeight;
+        const start = body.scrollTop;
+        const pieceMax = 7;
+        let dist = end - start;
+        if (dist <= 0) {
+            clearInterval(id);
+            return;
+        }
+        let piece = (dist < pieceMax) ? dist : pieceMax;
+        body.scrollTop = start + piece;
+    },0.01);
+
+};
+scrollLink.addEventListener('click', onClickOnScrollLink,false);
