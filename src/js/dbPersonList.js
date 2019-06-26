4d7c562699d4c2c02c720758b649bc0252193bc4
Sergei Mamykin <mamykin@oborot.ru>
2019-06-26 17:37:26 +0300

    Вынес получение store в отдельный файл и отрефакторил остальной код

---
+++ src/js/dbPersonList.js
@@ -0,0 +1,41 @@
+import {openDB} from 'idb';
+
+export default async function getIDBPersonList() {
+    const dbName = 'app';
+    const personListStoreName = 'person_list';
+    const version = 1;
+    const dbPromise = openDB(dbName, version, {
+        upgrade(db, oldVersion, newVersion, transaction) {
+            debugger;
+            for (let i = oldVersion; i < newVersion; ++i) {
+                switch (oldVersion) {
+                    case 0:
+                        db.createObjectStore(personListStoreName, {autoIncrement: true});
+                        break;
+                }
+            }
+        },
+        blocked() {
+        },
+        blocking() {
+        }
+    });
+
+    return {
+        async get(key) {
+            return (await dbPromise).get(personListStoreName, key);
+        },
+        async add(val) {
+            return (await dbPromise).add(personListStoreName, val);
+        },
+        async put(key, val) {
+            return (await dbPromise).put(personListStoreName, val, key);
+        },
+        async delete(key) {
+            return (await dbPromise).delete(personListStoreName, key);
+        },
+        async clear() {
+            return (await dbPromise).clear(personListStoreName);
+        }
+    };
+}
--- src/js/people_list.js
+++ src/js/people_list.js
@@ -1,104 +1,72 @@
 import tingle from 'tingle.js';
-import {openDB} from 'idb';
+import getIDBPersonList from "./dbPersonList";
 
-(function(){
-    if (location.pathname !== '/people_list.html') {
-        return;
-    }
+(function () {
 
     const addBtn = document.querySelector('.js-add_person');
-    if (!addBtn){
+    if (!addBtn) {
         return;
     }
 
-    async function getIDBPersonList(){
-        const dbName = 'app';
-        const personListStoreName = 'person_list';
-        const version = 1;
-        const dbPromise = openDB(dbName, version, {
-            upgrade(db, oldVersion, newVersion, transaction) {
-                debugger;
-                for (let i = oldVersion; i < newVersion; ++i){
-                    switch (oldVersion){
-                        case 0:
-                            db.createObjectStore(personListStoreName,{autoIncrement:true});
-                        break;
-                    }
-                }
-            },
-            blocked() {},
-            blocking() {}
-        });
-
-        return {
-            async get(key) {
-                return (await dbPromise).get(personListStoreName, key);
-            },
-            async add(val) {
-                return (await dbPromise).add(personListStoreName, val);
-            },
-            async put(key, val) {
-                return (await dbPromise).put(personListStoreName, val, key);
-            },
-            async delete(key) {
-                return (await dbPromise).delete(personListStoreName, key);
-            },
-            async clear() {
-                return (await dbPromise).clear(personListStoreName);
-            }
-        };
-    }
-
-    const onAddPerson = function(e){
+    const onAddPersonSubmit = function (e) {
         e.preventDefault();
         e.stopPropagation();
 
         let form = e.target,
             name = form['person_name'].value,
-            title= form['person_title'].value,
-            quote= form['person_quote'].value;
+            title = form['person_title'].value,
+            quote = form['person_quote'].value;
 
-        if (!name){
+        if (!name) {
             return;
         }
 
         const dbPersonList = getIDBPersonList();
 
-        dbPersonList.then(function(store){
-            store.add({ name, title, quote });
-        }).catch(function(reason){
+        dbPersonList.then(function (store) {
+            store.add({name, title, quote});
+        }).catch(function (reason) {
             console.error(reason);
         });
     };
 
+    const createAddForm = function () {
+        const wrapper = document.createElement('div');
+        const template = require('../templates/addPersonForm.twig');
+        wrapper.innerHTML = template().trim();
+
+        return wrapper.firstElementChild;
+    };
+
+    const createModal = function(){
+        return new tingle.modal({
+            footer: true,
+            stickyFooter: false,
+            closeMethods: ['overlay', 'button', 'escape'],
+            closeLabel: "Close",
+        });
+    };
+
+    const initAddFormModal = function () {
+
+        const modal = createModal();
+        const form = createAddForm();
+
+        form.addEventListener('submit', onAddPersonSubmit);
+        form.addEventListener('submit', function () {
+            modal.close()
+        });
+
+        modal.setContent(form);
+        return modal;
+    };
+    
     let addModal;
-    const showAddPersonModal = function() {
-
-        function initAddFormModal() {
-            let modal = new tingle.modal({
-                footer: true,
-                stickyFooter: false,
-                closeMethods: ['overlay', 'button', 'escape'],
-                closeLabel: "Close",
-            });
-
-
-            let div = document.createElement('div');
-            let template = require('../templates/addPersonForm.twig');
-            div.innerHTML = template();
-            let form = div.querySelector('form');
-            form.addEventListener('submit', onAddPerson);
-            form.addEventListener('submit', function(){
-                modal.close()
-            });
-            modal.setContent(div);
-            return modal;
-        }
-
+    const showAddPersonModal = function () {
         addModal = addModal || initAddFormModal();
         addModal.open();
     };
 
-    addBtn.addEventListener('click',showAddPersonModal)
+    addBtn.addEventListener('click', showAddPersonModal)
 })();
 
