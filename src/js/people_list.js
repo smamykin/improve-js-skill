523a5bd3be4ae86d11bce01f8e053b03d0e9ed7b
Sergei Mamykin <mamykin@oborot.ru>
2019-07-04 17:14:46 +0300

    убрал обертку так как она не нужна при использовании webpack

--- src/js/people_list.js
+++ src/js/people_list.js
@@ -6,154 +6,154 @@ import invite from "./Invitation";
 import ContextMenuFactory from "./ContextMenu";
 
 
-// add new person
-(function () {
+let _addModal;//use getModal()
 
-    let _addModal;//use getModal()
+const selectors ={
+        addBtn: '.js-add_person',
+        container: '.people_list_container'
+    },
+    addBtn = document.querySelector(selectors.addBtn),
+    dbPersonList = getIDBPersonList(),
+    loaderHtml = '<div class="loader-inner ball-beat"><div></div><div></div><div></div></div>',
+    getModal = () => {
+        return _addModal || initAddFormModal()
+    };
 
-    const body = document.querySelector('body'),
-        addBtn = document.querySelector('.js-add_person'),
-        dbPersonList = getIDBPersonList(),
-        loaderHtml = '<div class="loader-inner ball-beat"><div></div><div></div><div></div></div>',
-        getModal = () => {
-            return _addModal || initAddFormModal()
-        };
+//process
+refreshPeopleList();
+addBtn && addBtn.addEventListener('click', () => getModal().open());
 
-    //process
-    refreshPeopleList();
-    addBtn && addBtn.addEventListener('click', () => getModal().open());
+//process end
 
-    //process end
+/**
+ * pull from  db people list and render it into the page
+ */
+function refreshPeopleList() {
+    dbPersonList.getAll()
+        .then(preparePhotoUrls)
+        .then(renderPeople)
+        .then(initContextMenu)
+        .catch((reason) => console.error(reason))
+}
 
-    /**
-     * pull from  db people list and render it into the page
-     */
-    function refreshPeopleList() {
-        dbPersonList.getAll()
-            .then(preparePhotoUrls)
-            .then(renderPeople)
-            .then(initContextMenu)
-            .catch((reason) => console.error(reason))
+/**
+ * Because in the db is been storing File Objects we should convert it to string with url
+ *
+ * @param people
+ * @returns {*}
+ */
+async function preparePhotoUrls(people) {
+    for (let person of people) {
+        if (!person.photo) continue;
+
+        person.photo = window.URL.createObjectURL(person.photo);
     }
 
-    /**
-     * Because in the db is been storing File Objects we should convert it to string with url
-     *
-     * @param people
-     * @returns {*}
-     */
-    async function preparePhotoUrls(people) {
-        for (let person of people) {
-            if (!person.photo) continue;
+    return people;
+}
 
-            person.photo = window.URL.createObjectURL(person.photo);
-        }
+async function renderPeople(people) {
+    const container = document.querySelector(selectors.container);
 
-        return people;
-    }
+    container.innerHTML = require('./../templates/peopleList.twig')({'people': people});
 
-    async function renderPeople(people) {
-        const container = document.querySelector('.people_list_container');
+    return Array.from(container.children);
+}
 
-        container.innerHTML = require('./../templates/peopleList.twig')({'people': people});
+async function initContextMenu(personCards) {
+    personCards.forEach((element) => {
+        element.addEventListener('contextmenu', onCardContextMenu);
+    });
 
-        return Array.from(container.children);
-    }
+    return personCards;
+}
 
-    async function initContextMenu(personCards) {
-        personCards.forEach((element) => {
-            element.addEventListener('contextmenu', onCardContextMenu);
+function createAddForm() {
+    const html = require('./../templates/addPersonForm.twig')();
+    return getElementFromHtmlString(html);
+}
+
+function createModal() {
+    return new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeMethods: ['overlay', 'button', 'escape'],
+        closeLabel: "Close",
+    });
+}
+
+function initAddFormModal() {
+    const modal = createModal();
+    const form = createAddForm();
+
+    form.addEventListener('submit', onAddPersonSubmit);
+    form.addEventListener('submit', () => {
+        modal.close()
+    });
+
+    modal.setContent(form);
+
+    return modal;
+}
+
+function onAddPersonSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    checkForm(event.target)
+        .then(createPersonFromForm)
+        .then(dbPersonList.add)
+        .then(refreshPeopleList)
+        .catch((reason) => {
+            console.error(reason);
         });
+}
 
-        return personCards;
+async function checkForm(form) {
+    if (!form['person_name'].value) {
+        throw 'The field "name" should not be empty'
     }
 
-    function createAddForm() {
-        const html = require('./../templates/addPersonForm.twig')();
-        return getElementFromHtmlString(html);
+    return form;
+}
+
+async function createPersonFromForm(form) {
+    return {
+        name: form['person_name'].value,
+        title: form['person_title'].value,
+        quote: form['person_quote'].value,
+        photo: form['person_photo'].files[0]
+    };
+}
+
+function onCardContextMenu(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    const personId = +event.currentTarget.getAttribute('data-person_id'),
+        body = document.querySelector('body');
+
+    ContextMenuFactory
+        .create()
+        .setPosition(event.pageX, event.pageY)
+        .on('click', onInvite.bind(null, personId), true)
+        .addCloseEvents(['click', 'contextmenu'], body)
+        .show();
+}
+
+function onInvite(personId, event) {
+    if (!event.target.classList.contains('js-invite_btn')) {
+        return;
     }
 
-    function createModal() {
-        return new tingle.modal({
-            footer: true,
-            stickyFooter: false,
-            closeMethods: ['overlay', 'button', 'escape'],
-            closeLabel: "Close",
-        });
-    }
+    event.stopPropagation();
 
-    function initAddFormModal() {
-        const modal = createModal();
-        const form = createAddForm();
+    const loader = new Loader(event.target, loaderHtml);
 
-        form.addEventListener('submit', onAddPersonSubmit);
-        form.addEventListener('submit', () => {
-            modal.close()
-        });
-
-        modal.setContent(form);
-
-        return modal;
-    }
-
-    function onAddPersonSubmit(event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        checkForm(event.target)
-            .then(createPersonFromForm)
-            .then(dbPersonList.add)
-            .then(refreshPeopleList)
-            .catch((reason) => {
-                console.error(reason);
-            });
-    }
-
-    async function checkForm(form){
-        if (!form['person_name'].value){
-            throw 'The field "name" should not be empty'
-        }
-
-        return form;
-    }
-
-    async function createPersonFromForm(form) {
-        return {
-            name: form['person_name'].value,
-            title: form['person_title'].value,
-            quote: form['person_quote'].value,
-            photo: form['person_photo'].files[0]
-        };
-    }
-
-    function onCardContextMenu(event) {
-        event.preventDefault();
-        event.stopImmediatePropagation();
-
-        const personId = +event.currentTarget.getAttribute('data-person_id');
-
-        ContextMenuFactory
-            .create()
-            .setPosition(event.pageX, event.pageY)
-            .on('click', onInvite.bind(null, personId), true)
-            .addCloseEvents(['click', 'contextmenu'], body)
-            .show();
-    }
-
-    function onInvite(personId, event) {
-        if (!event.target.classList.contains('js-invite_btn')) {
-            return;
-        }
-
-        event.stopPropagation();
-
-        const loader = new Loader(event.target, loaderHtml);
-
-        loader.on();
-        invite(personId)
-            .then(() => alert('success'))
-            .catch((e) => alert(e))
-            .finally(() => loader.off());
-    }
-})();
+    loader.on();
+    invite(personId)
+        .then(() => alert('success'))
+        .catch((e) => alert(e))
+        .finally(() => loader.off());
+}
 
