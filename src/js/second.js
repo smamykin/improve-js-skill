bd59dba3c17281571414015ba5e3feb6bfd329b5
Sergei Mamykin <mamykin@oborot.ru>
2019-09-02 19:58:14 +0300

    потому что этот шаблон нужет js скрипту и не имеет ничего общего с основной html разметкой

--- src/js/second.js
+++ src/js/second.js
@@ -1,5 +1,5 @@
 setTimeout(() => console.log('hi'), 3000);
 
 let n  = document.createElement('div');
-n.innerHTML = require('../templates/new.twig')();
+n.innerHTML = require('./templates/new.twig')();
 document.body.appendChild(n);
